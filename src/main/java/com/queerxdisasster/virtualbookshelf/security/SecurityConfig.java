0a293c921f8a8e07b26bc2de2a7503ab8b4ad633
queerxdisasster <7germania7@gmail.com>
2024-05-20 04:12:48 +0300

    л

--- src/main/java/com/queerxdisasster/virtualbookshelf/security/SecurityConfig.java
+++ src/main/java/com/queerxdisasster/virtualbookshelf/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://176.109.106.12:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
